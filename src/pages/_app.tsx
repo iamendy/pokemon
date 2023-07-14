456d4b651b00e64e286aed30ecf0d5912f60d25e
Nnamdi <17215830+iamendy@users.noreply.github.com>
2023-07-14 11:49:34 +0100

    Add font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Manrope } from "next/font/google";
+
+const manrope = Manrope({
+  weight: ["400", "600", "700"],
+  variable: "--manrope",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${manrope.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
